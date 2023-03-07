3b359f0f24d1d344efdbd3ecc6721d10ec6b859c
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-03-07 18:13:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the Zeros/count-the-zeros.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+        // code hereint cn
+        int cnt=count(arr, arr+n, 0);
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
