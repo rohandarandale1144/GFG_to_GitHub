0bfcbe654e1bdc6eda1cad940cf8571f4ffe428e
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-03-14 23:56:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Facing the sun/facing-the-sun.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Returns count buildings that can see sunlight
+	int countBuildings(int h[], int n) {
+	    // code here
+	    int count=0, currMax=0;
+
+        for(int i=0; i<n; i++)
+
+        {
+
+            if(h[i]>currMax)
+
+            {
+
+                currMax = h[i];
+
+                count++;
+
+            }
+
+        }
+
+        return count;
+
+ }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int h[n];
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+        }
+        Solution ob;
+        auto ans = ob.countBuildings(h, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
