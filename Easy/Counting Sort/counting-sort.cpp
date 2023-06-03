a32ab9ca49d06f8876754c026c0fa401d0744093
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-06-03 15:32:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Counting Sort/counting-sort.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+#define RANGE 255
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to arrange all letters of a string in lexicographical 
+    //order using Counting Sort.
+    string countSort(string arr){
+        // code here
+        sort(arr.begin(), arr.end());
+        string ans="";
+        for(int i=0;i<arr.size();i++)
+        {
+            ans+=arr[i];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr;
+        cin>>arr;
+        Solution obj;
+        cout<<obj.countSort(arr)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
