b0a78bb8a9a123380d16cf07862ae07682c79e41
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-09-12 07:51:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/Perfect Numbers/perfect-numbers.cpp
+++ Easy/Perfect Numbers/perfect-numbers.cpp
@@ -7,18 +7,19 @@ class Solution {
   public:
     int isPerfectNumber(long long N) {
         // code here
-        int sum=1;
-        for(int i=2;i<=sqrt(N);i++){
+        if(N==1)
+        return 0;
+        
+        long long sum=1;
+        
+        for(long long i=2;i*i<N;i++)
+        {
             if(N%i==0)
             {
-                if(i!=N)
-                sum+=i+(N/i);
+                sum=sum+i+(N/i);
             }
         }
-        if(sum==N && N!=1)
-        return 1;
-        else
-        return 0;
+        return sum==N;
     }
 };
 
