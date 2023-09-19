a8720ef85a46794effa4b0ef029ca75ebc44ada1
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-09-19 17:17:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find first set bit/find-first-set-bit.cpp
+++ Easy/Find first set bit/find-first-set-bit.cpp
@@ -16,7 +16,7 @@ class Solution
     unsigned int getFirstSetBit(int n)
     {
         // Your code here
-        if(n==0)
+         if(n==0)
             return 0;
         int count = 0;
         while(n>0)
