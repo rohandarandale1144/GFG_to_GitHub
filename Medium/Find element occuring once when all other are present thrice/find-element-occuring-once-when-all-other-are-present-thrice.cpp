db4a4f1ad2e8aaa17a8c5dc1f061562890ed58f9
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2024-01-04 00:40:09 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -12,17 +12,17 @@ class Solution {
   public:
     int singleElement(int arr[] ,int N) {
         // code here
-        unordered_map<int, int>mp;
-        for(int i=0;i<N;i++)
-        {
+        int ans;
+        map<int, int>mp;
+        for(int i=0;i<N;i++){
             mp[arr[i]]++;
         }
-        for(auto i:mp)
-        {
-            if(i.second==1)
-            return i.first;
+        for(auto it:mp){
+            if(it.second==1){
+                ans=it.first;
+            }
         }
-        
+        return ans;
     }
 };
 
