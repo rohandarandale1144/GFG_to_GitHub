71e74222e12e0dd78c3eff597b321dcc848902e4
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2024-03-05 00:16:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.cpp
+++ Medium/Maximum Index/maximum-index.cpp
@@ -1,50 +1,57 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 class Solution{
-public:
-    int maxIndexDiff(int arr[], int n) {
-        // code here
-        int ans;
-        int maxi=0;
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int a[], int n) 
+    { 
+        // Your code here
+        int c=0;
         for(int i=0;i<n;i++)
         {
-            int j=n-1;
-            while(j>=i)
+            for(int j=1;j<n;j++)
             {
-                if(arr[j]>=arr[i])
+                if(a[i]<=a[j]&&c<(j-i))
                 {
-                    maxi=max(maxi, j-i);
-                    break;
+                    c=j-i;
                 }
-                j--;
             }
+            if(c==n-1-i) return c;
         }
-        return maxi;
+        return c;
     }
-
 };
 
-
 //{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, i;
-        cin >> n;
-        int a[n];
-        for (i = 0; i < n; i++) {
-            cin >> a[i];
-        }
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
         Solution ob;
-        auto ans = ob.maxIndexDiff(a, n);
-        cout << ans << "\n";
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
     }
     return 0;
-}
+} 
 // } Driver Code Ends
