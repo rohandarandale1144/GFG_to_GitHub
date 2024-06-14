ea30730ecaef5cab9acb91c45fd671c5236f1379
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2024-06-14 11:44:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n) {
+        // code here
+        int m=n;
+        int sum=0;
+        int rem=0;
+        while(m>0){
+            rem=m%10;
+            int power=pow(rem, 3);
+            sum+=power;
+            m/=10;
+        }
+        if(sum==n)
+        return "true";
+        else
+        return "false";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
