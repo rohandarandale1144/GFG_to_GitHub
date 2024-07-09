9b73556e57da7544200d78cf037a91ea6496896d
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2024-07-09 10:07:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiply 2 matrices/multiply-2-matrices.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    void Multiply(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+        int m=matrixA.size();
+        int n=matrixA[0].size();
+        vector<vector<int>>c(m, vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                c[i][j]=0;
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    c[i][j]+=matrixA[i][k]*matrixB[k][j];
+                }
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrixA[i][j]=c[i][j];
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrixA(n, vector<int>(n,0));
+		vector<vector<int>> matrixB(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixA[i][j];
+			}
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixB[i][j];
+			}
+		}
+		Solution ob;
+		ob.Multiply(matrixA, matrixB);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cout << matrixA[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
