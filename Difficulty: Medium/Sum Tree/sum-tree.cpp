95726ce241a31c0ba11289056d13c3e971fd6316
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2024-08-08 00:10:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -2,83 +2,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node {
     int data;
-    struct Node *left;
-    struct Node *right;
+    struct Node* left;
+    struct Node* right;
 };
+
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
+
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
@@ -109,32 +108,33 @@ int check(Node*root, bool *ans){
     int total=left+right+root->data;
     return total;
 }
-class Solution
-{
-    public:
-    bool isSumTree(Node* root)
-    {
-         // Your code here
-         bool flag=true;
-         check(root, &flag);
-         return flag;
+class Solution {
+  public:
+    bool isSumTree(Node* root) {
+        // Your code here
+        bool flag=true;
+        check(root, &flag);
+        return flag;
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
 
     int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-		getline(cin,s);
+
+        getline(cin, s);
+
         Node* root = buildTree(s);
         Solution ob;
-        cout <<ob.isSumTree(root) << endl;
+        if (ob.isSumTree(root))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
     }
     return 1;
 }
